94d43ebb1aacb4f30e64103b6b1fbd196e4bdcb9
celsosoares31 <celsosoares@sapo.mz>
2023-02-07 20:35:46 +0200

    Codigo final do capitulo 15

---
+++ client/app/util/ConnectionFactory.js
@@ -0,0 +1,45 @@
+const ConnectionFactory = (() => {
+  const stores = ["negociacoes"];
+  let connection = null;
+  let close = null;
+
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error("Nao e possivel criar instancias desta classe");
+    }
+    static _createStores(connection) {
+      stores.forEach((store) => {
+        if (connection.objectStoreNames.contains(store)) {
+          connection.deleteObjectStore(store);
+        }
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    }
+    static closeConnection() {
+      if (connection) close();
+    }
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        if (connection) return resolve(connection);
+        const openRequest = indexedDB.open("cangaceiroDB", 1);
+
+        openRequest.onupgradeneeded = (e) => {
+          ConnectionFactory._createStores(e.target.result);
+        };
+
+        openRequest.onsuccess = (e) => {
+          connection = e.target.result;
+          close = connection.close.bind(connection);
+          connection.close = () => {
+            throw new Error("Voce nao pode fechar a conexao directamente");
+          };
+          resolve(e.target.result);
+        };
+
+        openRequest.onerror = (e) => {
+          reject(e.target.error.name);
+        };
+      });
+    }
+  };
+})();
